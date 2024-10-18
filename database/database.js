ffce4c8e132b32b60468fc566239306122e0648b
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-10-18 12:37:14 +0000

    database test

--- database/database.js
+++ database/database.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose')
 const {MONGO_URI} = process.env
 
 exports.connect = () => {
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then()
+    mongoose.connect(MONGO_URI)
+    .then(
+        console.log("DATABASE IS CONNECTED SUCCESSFULLY")
+    )
     .catch((error) => {
         console.log('DB CONNECTION ERROR');
         console.log(error);
